49aca8efa3d9fe1336a14485e33897d8eff9b4b0
kritipareek <kkritipareek@gmail.com>
2026-07-27 03:32:14 +0000

    Day 1: print name function

--- DSA/day1.cpp
+++ DSA/day1.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h> 
 using namespace std;
 //functions
+void printname(string name){
+    cout<<"hey "<<name;
+}
 int main() {
+    string name;
+    cin>>name;
+    printname (name);
+
+    string name2;
+    cin >> name2;
+    printname(name2);
     
     return 0;
 }
